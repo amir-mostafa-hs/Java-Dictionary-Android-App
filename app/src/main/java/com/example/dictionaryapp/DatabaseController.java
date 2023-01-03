116426aafd26bc6a13bbcdac2b522e95b11875ce
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-01-04 01:24:09 +0330

    create database variable

--- app/src/main/java/com/example/dictionaryapp/DatabaseController.java
+++ app/src/main/java/com/example/dictionaryapp/DatabaseController.java
@@ -3,10 +3,26 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/*
+    This class create for create and connect
+    to the sqlite database and save app data
+*/
 public class DatabaseController extends SQLiteOpenHelper {
+    // save database name and version in to variable
     private static final String dbName = "Dictionary";
+    private static final Integer dbVersion = 1;
+    // save database table name in to variable
+    private static final String dbTableName = "WordsTable";
+    // save database table columns name in to variable
+    private static final String dbTableColumnWordId = "WordId";
+    private static final String dbTableColumnWord = "Word";
+    private static final String dbTableColumnPersianTranslate = "PersianTranslate";
+    private static final String dbTableColumnArabicTranslate = "ArabicTranslate";
+    private static final String dbTableColumnPronounce = "Pronounce";
+    private static final String dbTableColumnDescriptions = "Descriptions";
+
     public DatabaseController(Context context) {
-        super(context, name, factory, version);
+        super(context, dbName, null, dbVersion);
     }
 
     @Override
