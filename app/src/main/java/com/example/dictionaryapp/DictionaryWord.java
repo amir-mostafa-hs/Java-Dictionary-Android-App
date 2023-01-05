675c06d24dfff742696448d7e031ebfd6493e669
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2023-01-05 14:06:22 +0330

    edit DictionaryWord class for add constructor method

--- app/src/main/java/com/example/dictionaryapp/DictionaryWord.java
+++ app/src/main/java/com/example/dictionaryapp/DictionaryWord.java
@@ -6,4 +6,13 @@ public class DictionaryWord {
     public String ArabicTranslate;
     public String Pronounce;
     public String Descriptions;
+
+    // create constructor method for set class properties value
+    public DictionaryWord(String word,String persianTranslate,String arabicTranslate,String pronounce,String descriptions) {
+        this.Word = word;
+        this.PersianTranslate = persianTranslate;
+        this.ArabicTranslate = arabicTranslate;
+        this.Pronounce = pronounce;
+        this.Descriptions = descriptions;
+    }
 }
